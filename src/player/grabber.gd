4cfc74f1f156783f3c8d2e68de68a135d158c6b6
Victor <victfv@gmail.com>
2025-08-09 00:15:36 -0300

    Added some furniture, grab rotation speed

--- src/player/grabber.gd
+++ src/player/grabber.gd
@@ -2,8 +2,10 @@ extends Generic6DOFJoint3D
 
 class_name PlayerGrabber
 @export var player : Node3D
+@export var rotator: PlayerRotator
 
 var grab_node : Node3D
+
 func grab(obj : DynamicObject) -> void:
 	#print("grab")
 	if obj:
@@ -12,6 +14,7 @@ func grab(obj : DynamicObject) -> void:
 		global_rotation = grab_node.global_rotation
 		node_a = get_path_to(player)
 		node_b = get_path_to(grab_node)
+		rotator.speed = clamp(remap(grab_node.mass, 1.0, 50.0, 2.0, 0.25),0.5,6.0)
 		#print(node_a," ", node_b)
 
 func release(obj : DynamicObject) -> void:
@@ -20,4 +23,5 @@ func release(obj : DynamicObject) -> void:
 		#grab_core_joint.node_b = NodePath()
 		node_a = NodePath()
 		node_b = NodePath()
+		rotator.speed = 6.0
 		grab_node = null
--- src/player/rotator.gd
+++ src/player/rotator.gd
@@ -4,10 +4,11 @@ class_name PlayerRotator
 @export var player : Player
 @export var input_receiver: InputReceiver
 @export var head: Node3D
+var speed : float = 6.0
 
 const hpi : float = PI / 2.0
 func _physics_process(delta: float) -> void:
-	player.global_rotation.y = lerp_angle(player.global_rotation.y,-input_receiver.l_udlr.angle() - hpi,delta * 6.0)
+	player.global_rotation.y = lerp_angle(player.global_rotation.y,-input_receiver.l_udlr.angle() - hpi,delta * speed)
 	head.global_rotation.y = lerp_angle(head.global_rotation.y,-input_receiver.l_udlr.angle() - hpi,delta * 16.0)
 	#print(head.rotation.y)
 	head.rotation.y = clampf(head.rotation.y, -hpi, hpi)
